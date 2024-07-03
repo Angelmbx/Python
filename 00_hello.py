4e09d9452608bcdef35b1c1fa3a06d4254d6427b
Angelmbx <mirandabl.angel@gmail.com>
2024-07-03 13:39:15 +0200

    First commit

---
+++ 00_hello.py
@@ -0,0 +1,12 @@
+# Hola mundo
+print ('Hola Python')
+
+"""
+Este es un
+comentario
+en varias lineas
+"""
+
+#Consultar el tipo de dato
+print(type("Esto es un String!")) # Tipo 'str'
+print(type(343.83)) # Tipo 'float'
---
+++ 01_variables.py
@@ -0,0 +1,9 @@
+# Variables
+
+MyVariable = "New string"
+print(MyVariable)
+
+# convencion = escribir las variables en snake_case
+
+my_variable = "Really new string"
+print(my_variable)
