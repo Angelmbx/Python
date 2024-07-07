d35a56b4f047de74fd43a36d74c66f4b1cfc6650
Angelmbx <mirandabl.angel@gmail.com>
2024-07-07 22:47:50 +0200

    Conditionals finished

---
+++ 08_conditionals.py
@@ -0,0 +1,18 @@
+### Conditionals ###
+
+my_condition = False
+
+if my_condition:
+    print("Se ejecuta la condición del if")
+
+
+my_condition = 8 * 2 + 4    
+
+if my_condition > 15 and my_condition < 20:
+    print("Es mayor de 15 y menor que 20")
+elif my_condition == 20:
+    print("El número es 20")
+else:
+    print ("Es menor o igual a 15 o mayor que 20")
+
+print ("La ejecución continúa")
